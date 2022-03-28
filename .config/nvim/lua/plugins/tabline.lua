2b475fb5df168ef13dedd04f420426c13a813f18
theRealCarneiro <gabriel.chaves.carneiro@gmail.com>
2022-03-28 14:14:06 -0300

    update tabline plugin

--- .config/nvim/lua/plugins/tabline.lua
+++ .config/nvim/lua/plugins/tabline.lua
@@ -1,7 +1,9 @@
 require('tabline').setup({
-    show_index = false,       -- show tab index
-    show_modify = true,       -- show buffer modification indicator
-	bracket_character = false,  -- show brackets in name
-    modify_indicator = '[+]', -- modify indicator
-    no_name = '[No name]',    -- no name buffer name
+    show_index = false,        -- show tab index
+    show_modify = true,        -- show buffer modification indicator
+	bracket_character = false, -- show brackets in name
+	showtabline = 1,		   -- 1 to hide tabline when only 1 buffer, 2 to show
+	hide_single_buffer = true, -- hide tabline when there's only one buffer
+    modify_indicator = '[+]',  -- modify indicator
+    no_name = '[No name]',     -- no name buffer name
 })
